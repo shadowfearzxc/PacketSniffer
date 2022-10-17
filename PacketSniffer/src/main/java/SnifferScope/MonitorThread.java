d92586113568325c9d714ca9b06fc939520c357b
shadowfearzxc <kurilik.a.12@mail.ru>
2022-10-17 16:24:30 +0300

    v1.0.0

---
+++ PacketSniffer/src/main/java/SnifferScope/MonitorThread.java
@@ -0,0 +1,131 @@
+package SnifferScope;
+
+import java.io.File;
+//import java.net.Inet4Address;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+//import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.pcap4j.core.BpfProgram.BpfCompileMode;
+//import org.pcap4j.core.NotOpenException;
+//import org.pcap4j.core.PacketListener;
+//import org.pcap4j.core.PcapAddress;
+import org.pcap4j.core.PcapDumper;
+import org.pcap4j.core.PcapHandle;
+//import org.pcap4j.core.PcapHandle.TimestampPrecision;
+//import org.pcap4j.core.PcapNativeException;
+import org.pcap4j.core.PcapNetworkInterface;
+import org.pcap4j.core.PcapNetworkInterface.PromiscuousMode;
+import org.pcap4j.core.PcapStat;
+//import org.pcap4j.core.Pcaps;
+//import org.pcap4j.packet.IpV4Packet;
+import org.pcap4j.packet.Packet;
+//import org.pcap4j.util.NifSelector;
+//import org.pcap4j.util.Packets;
+//import org.pcap4j.util.PropertiesLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//import program.gui.WireScopeMain;
+
+//import javax.swing.*;
+
+public class MonitorThread extends Thread {
+
+    final static Logger logger = LoggerFactory.getLogger(MonitorThread.class);
+    LinkedBlockingQueue<Packet> packetQueue;
+    PcapNetworkInterface inteface;
+    volatile AtomicBoolean running = new AtomicBoolean();
+    boolean fileOutput = false;
+    File file = null;
+    //JButton btnStart, btnStop;
+
+
+    String filter;
+
+    public MonitorThread(LinkedBlockingQueue<Packet> packetQueue, PcapNetworkInterface inteface, String filter) {
+        this.packetQueue = packetQueue;
+        this.inteface = inteface;
+        this.filter = filter;
+        running.set(true);
+    }
+
+    /**
+     проверка на существование дамп файла + на англ языке
+     */
+    public void setFileOutput(File file) {
+        this.file = file;
+        fileOutput = true;
+    }
+
+    /**
+     * остановка процесса
+     */
+    public void StopUpdating() {
+        running.set(false);
+    }
+
+    @Override
+    public void run() {
+        PcapDumper dumper = null;
+
+        try {
+            /** установка максимального размера пакета с таймаутом в 1 секунду */
+            final PcapHandle handle = inteface.openLive(65536, PromiscuousMode.PROMISCUOUS, 1000);
+
+            handle.setFilter(filter, BpfCompileMode.OPTIMIZE);
+
+            if (fileOutput) {
+                dumper = handle.dumpOpen(file.getAbsolutePath());
+            }
+
+            int num = 0; /** объявление переменной для счетчика количества пакетов */
+            while (running.get()) {
+                Packet packet = handle.getNextPacket();
+
+                if (packet == null) {
+                    continue;
+                } else {
+                    packetQueue.add(packet);
+
+                    if (fileOutput) {
+                        dumper.dump(packet, handle.getTimestamp());
+                    }
+
+                    logger.debug(handle.getTimestamp().toString());
+                    logger.debug(packet.toString());
+
+                    num++;
+                    if (num >= 10000) { /** максимальное значение для перехвата количества пакетов */
+                        //btnStart.setEnabled(true);
+                        //btnStop.setEnabled(false);
+                        break;
+                    }
+                }
+            }
+/**
+ * финальное сообщение после окончания перехвата пакетов
+ */
+            PcapStat ps = handle.getStats();
+            logger.info("Пакетов получено: " + ps.getNumPacketsReceived());
+            logger.info("Пакетов потеряно: " + ps.getNumPacketsDropped());
+            logger.info("Пакетов потеряно интерфейсом: " + ps.getNumPacketsDroppedByIf());
+            if (com.sun.jna.Platform.isWindows()) {
+                logger.info("Пакетов захвачено: " + ps.getNumPacketsCaptured());
+            }
+
+            if (fileOutput) {
+                dumper.close();
+            }
+            handle.close();
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ PacketSniffer/src/main/java/SnifferScope/UpdateTextOutput.java
@@ -0,0 +1,135 @@
+package SnifferScope;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import org.pcap4j.packet.ArpPacket;
+import org.pcap4j.packet.DnsPacket;
+import org.pcap4j.packet.IcmpV4CommonPacket;
+import org.pcap4j.packet.IpV4Packet;
+import org.pcap4j.packet.IpV6Packet;
+import org.pcap4j.packet.Packet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UpdateTextOutput extends Thread {
+
+    final static Logger logger = LoggerFactory.getLogger(UpdateTextOutput.class);
+
+    volatile AtomicBoolean running = new AtomicBoolean();
+    DefaultTableModel tableModel;
+    LinkedBlockingQueue<Packet> packetQueue;
+    JTable table;
+
+    public UpdateTextOutput(LinkedBlockingQueue<Packet> packetQueue, DefaultTableModel tableModel, JTable table) {
+        this.tableModel = tableModel;
+        this.packetQueue = packetQueue;
+        this.table = table;
+        running.set(true);
+    }
+
+    public void StopUpdating() {
+        running.set(false);
+    }
+
+    @Override
+    public void run() {
+        int num = 0; /** счетчик пакетов */
+        boolean changed = false;
+
+        /** выполнение потока до тех пор, пока очередь не станет пустой*/
+        while (running.get() || !packetQueue.isEmpty()) {
+            try {
+                changed = false;
+
+
+
+                /** не работает вывод в строки*/
+                while (!packetQueue.isEmpty()) {
+                    if (tableModel.getRowCount() > 10000) { /**после 1000 строки удаляется самая первая по циклу*/
+                        tableModel.removeRow(0);
+                    }
+
+                    /** статические пакеты */
+                    Packet packet = packetQueue.poll();
+                    num++;
+
+                    if (packet.contains(IpV4Packet.class)) {
+                        IpV4Packet ip4v = packet.get(IpV4Packet.class); // IPv4
+
+                        if (ip4v.getPayload().contains(DnsPacket.class)) {
+                            DnsPacket dns = ip4v.getPayload().get(DnsPacket.class);
+
+                            tableModel.addRow(new Object[] {num,   "DNS", ip4v.getHeader().getSrcAddr(),
+                                    ip4v.getHeader().getDstAddr(), dns.toString() });
+                            //   logger.info("" + packet);
+                        } else if (ip4v.getPayload().contains(IcmpV4CommonPacket.class)) {
+                            IcmpV4CommonPacket icmp = ip4v.getPayload().get(IcmpV4CommonPacket.class);
+                            //  logger.info("" + packet);
+
+                            tableModel.addRow(new Object[] {num,   "ICMP", ip4v.getHeader().getSrcAddr(),
+                                    ip4v.getHeader().getDstAddr(), icmp.toString() });
+                            //  logger.info("" + packet);
+
+                        } else {
+                            tableModel.addRow(new Object[] {num,   "IPv4", ip4v.getHeader().getSrcAddr(),
+                                    ip4v.getHeader().getDstAddr(), ip4v.toString() });
+                            //   logger.info("ТИП - [IPv4] " + packet);
+                            //  logger.info("" + packet);
+
+                        }
+
+                    } else if (packet.contains(IpV6Packet.class)) {
+                        IpV6Packet ip6v = packet.get(IpV6Packet.class); /** Ipv6*/
+
+                        if (ip6v.getPayload().contains(DnsPacket.class)) {
+                            DnsPacket dns = ip6v.getPayload().get(DnsPacket.class);
+
+                            tableModel.addRow(new Object[] {num,   "DNS", ip6v.getHeader().getSrcAddr(),
+                                    ip6v.getHeader().getDstAddr(), dns.toString() });
+                            //  logger.info("" + packet);
+                        } else if (ip6v.getPayload().contains(IcmpV4CommonPacket.class)) {
+                            IcmpV4CommonPacket icmp = ip6v.getPayload().get(IcmpV4CommonPacket.class);
+
+                            tableModel.addRow(new Object[] {num,   "ICMP", ip6v.getHeader().getSrcAddr(),
+                                    ip6v.getHeader().getDstAddr(), icmp.toString() });
+                            // logger.info("" + packet);
+
+                        } else {
+                            tableModel.addRow(new Object[] {num,   "IPv6", ip6v.getHeader().getSrcAddr(),
+                                    ip6v.getHeader().getDstAddr(), ip6v.toString() });
+                            // logger.info("" + packet);
+                        }
+                    } else if (packet.contains(ArpPacket.class)) { /** ARP низкий уровень пакета, не будет найдет из-за ipv4 ipv6*/
+                        ArpPacket arp = packet.get(ArpPacket.class);
+                        tableModel.addRow(new Object[] {num,  "ARP", arp.getHeader().getSrcHardwareAddr(),
+                                arp.getHeader().getDstHardwareAddr(), arp.toString() });
+                        // logger.info("" + packet);
+                    } else {
+                        // logger.info("Неизвестный тип пакета ");
+                        logger.info("\n  Пакет с номером [" + num + "] " + "\n  ТИП - [Неизвестный тип пакета] " + packet);
+                        tableModel.addRow(new Object[] {num, "UNKNOWN Type", packet.getHeader(), packet.getHeader(), packet.toString() } );
+                    }
+
+                    changed = true;
+                }
+                if (changed) {
+                    table.getParent().revalidate(); /** обновляет страницу, на гитхабе посмотреть решение*/
+                }
+                try {
+                    Thread.sleep(250);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
