9ed56cad57deafdff14f1d7a04ab1e64187614ec
shadowfearzxc <kurilik.a.12@mail.ru>
2022-10-18 10:14:59 +0300

    Added filters. Fix's. Optimization.

--- PacketSniffer/src/main/java/gui/PacketSniffer.java
+++ PacketSniffer/src/main/java/gui/PacketSniffer.java
@@ -23,8 +23,7 @@ import SnifferScope.MonitorThread;
 import SnifferScope.UpdateTextOutput;
 
 public class PacketSniffer {
-    //static Frame FirstFrame = new Frame();
-    //tatic String title = "PacketSniffer";
+
 
 
     final static Logger logger = LoggerFactory.getLogger(PacketSniffer.class);
@@ -53,7 +52,6 @@ public class PacketSniffer {
 
     JTextArea txtData;
 
-    private LinkedBlockingQueue<Packet> packetQueue;
     private JTable tblOutput;
     private DefaultTableModel tableModel;
 
@@ -61,14 +59,12 @@ public class PacketSniffer {
      * Запуск приложения.
      */
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    PacketSniffer window = new PacketSniffer(); /** основной интерфейс */
-                    window.frame.setVisible(true); /** включение-выключение отображения интерфейса, если false - не работает */
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        EventQueue.invokeLater(() -> {
+            try {
+                PacketSniffer window = new PacketSniffer(); // основной интерфейс */
+                window.frame.setVisible(true); //* включение-выключение отображения интерфейса, если false - не работает */
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
     }
@@ -76,7 +72,7 @@ public class PacketSniffer {
     /**
      * Создание приложения
      */
-    public PacketSniffer() { /** поиск всех возможный адаптеров и их вывод, работает библиотека pcap4j */
+    public PacketSniffer() { // поиск всех возможный адаптеров и их вывод, работает библиотека pcap4j */
         initialize();
         try {
             interfaces = org.pcap4j.core.Pcaps.findAllDevs();
@@ -87,38 +83,38 @@ public class PacketSniffer {
         }
     }
 
-    private void populateInterfaceList() { /** вывод в консоль список адаптеров */
+    private void populateInterfaceList() { //* вывод в консоль список адаптеров */
         for (PcapNetworkInterface inFace : interfaces) {
             ddlInterfaces.addItem(new ComboItem(inFace.getDescription(), inFace.getName()));
             logger.info("Адаптер: " + inFace.getDescription());
         }
     }
 
-    private void StartMonitoring() { /** после запуска, выводит IP адрес исполнительного адаптера */
-        packetQueue = new LinkedBlockingQueue<Packet>();
+    private void StartMonitoring() { // после запуска, выводит IP адрес исполнительного адаптера */
+        LinkedBlockingQueue<Packet> packetQueue = new LinkedBlockingQueue<Packet>();
 
         try {
             PcapNetworkInterface inteface = interfaces.get(ddlInterfaces.getSelectedIndex());
 
             for (PcapAddress addr : inteface.getAddresses()) {
-                if (addr.getAddress() != null) { /** сам вывод адреса исполнительного адаптера */
+                if (addr.getAddress() != null) { //* сам вывод адреса исполнительного адаптера */
                     logger.info("IP адрес: " + addr.getAddress());
                 }
             }
 
             monitorThread = new MonitorThread(packetQueue, inteface, txtFilters.getText());
 
-            if (chkBoxDumpFile.isSelected()) { /** если нажали на кнопку дамп файл ВКЛЮЧИТЬ, открытие окна выбора файла */
+            if (chkBoxDumpFile.isSelected()) { //* если нажали на кнопку дамп файл ВКЛЮЧИТЬ, открытие окна выбора файла */
                 File file = new File(lblFilePath.getText());
                 monitorThread.setFileOutput(file);
             }
 
-            monitorThread.start(); /** старт поиска пакетов */
+            monitorThread.start(); //* старт поиска пакетов */
 
             updateTextOutputThread = new UpdateTextOutput(packetQueue, tableModel, tblOutput);
             updateTextOutputThread.start();
-            btnStart.setEnabled(false); /** выключение кнопки старта */
-            btnStop.setEnabled(true); /** включение кнопки стоп */
+            btnStart.setEnabled(false); //* выключение кнопки старта */
+            btnStop.setEnabled(true); //* включение кнопки стоп */
 
         } catch (Exception e) {
             // TODO Auto-generated catch block
@@ -133,107 +129,99 @@ public class PacketSniffer {
     private void initialize() {
         frame = new JFrame();
         frame.setTitle("PacketSniffer v1.0.11");
-        frame.setBounds(100, 100, 708, 496); /** выставление начальных размеров окна */
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); /** операция закрытия */
-        frame.getContentPane().setLayout(new BorderLayout(0, 0)); /** панель содержимого */
-/** gitgub interface JPanel*/
-        JPanel pnlSide = new JPanel(); /** объявление панели 1  для интерфейса. */
-        pnlSide.setLayout(new BoxLayout(pnlSide, BoxLayout.Y_AXIS)); /** выстраивание панелей по Y (вертикаль) */
+        frame.setBounds(100, 100, 708, 496); //* выставление начальных размеров окна */
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // операция закрытия */
+        frame.getContentPane().setLayout(new BorderLayout(0, 0)); // панель содержимого */
+// gitgub interface JPanel*/
+        JPanel pnlSide = new JPanel(); // объявление панели 1  для интерфейса. */
+        pnlSide.setLayout(new BoxLayout(pnlSide, BoxLayout.Y_AXIS)); // выстраивание панелей по Y (вертикаль) */
 
-        JPanel panel_1 = new JPanel(); /** объявление панели 2 для интерфеса */
+        JPanel panel_1 = new JPanel(); // объявление панели 2 для интерфеса */
         pnlSide.add(panel_1);
-        panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS)); /** выставление списка адаптеров под кнопками с макетом */
+        panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS)); //* выставление списка адаптеров под кнопками с макетом */
 
-        btnStart = new JButton("\u25B6");           /**значок стрелочки */
+        btnStart = new JButton("\u25B6");           //*значок стрелочки */
         btnStart.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
-                StartMonitoring(); /** запуск функции для перехвата пакетов */
+                StartMonitoring(); // запуск функции для перехвата пакетов */
             }
         });
-        panel_1.add(btnStart); /** добавление кнопки старт в панель */
+        panel_1.add(btnStart); //* добавление кнопки старт в панель */
         btnStart.setForeground(new Color(0, 128, 0));
 
-        btnStop = new JButton("\u25A0");        /** значок стоп*/
+        btnStop = new JButton("\u25A0");        // значок стоп*/
         btnStop.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
 
-                monitorThread.StopUpdating(); /** запуск функции останавливающая весь процесс перехвата пакетов
-                 вывод сообщения с информацией о записи/потери пакетов
-                 кнопка старт активируется - кнопка стоп деактивация */
+                monitorThread.StopUpdating(); //* запуск функции останавливающая весь процесс перехвата пакетов
+               //  вывод сообщения с информацией о записи/потери пакетов
+                // кнопка старт активируется - кнопка стоп деактивация */
                 updateTextOutputThread.StopUpdating();
                 btnStart.setEnabled(true);
                 btnStop.setEnabled(false);
             }
         });
-        btnStop.setEnabled(false); /** первоначальное состояние кнопки - выключено */
+        btnStop.setEnabled(false); // первоначальное состояние кнопки - выключено */
 
         panel_1.add(btnStop);
         btnStop.setForeground(new Color(255, 0, 0));
 
-        JPanel panel_2 = new JPanel(); /** создание окошка для выбора доступных адаптеров */
+        JPanel panel_2 = new JPanel(); // создание окошка для выбора доступных адаптеров */
         pnlSide.add(panel_2);
         panel_2.setLayout(new FlowLayout(FlowLayout.LEADING, 5, 5));
 
         JLabel lblInterface = new JLabel("Адаптеры: ");
         panel_2.add(lblInterface);
 
-        ddlInterfaces = new JComboBox(); /** совмещение панелей */
+        ddlInterfaces = new JComboBox(); //* совмещение панелей */
         panel_2.add(ddlInterfaces);
 
-        JPanel panel_3 = new JPanel(); /** не работает */
+        JPanel panel_3 = new JPanel(); //* не работает */
         pnlSide.add(panel_3);
 
-        { /**фильтры не работают
-         P.S. сайт синтаксисов фильтров для работы с библиотекой
-         http://biot.com/capstats/bpf.html*/
-            //JLabel lblArguments = new JLabel("Фильтры");
-            // panel_3.add(lblArguments);
+        { //*фильтры
+         //P.S. сайт синтаксисов фильтров для работы с библиотекой
+         //http://biot.com/capstats/bpf.html*/
+            JLabel lblArguments = new JLabel("Фильтры");
+             panel_3.add(lblArguments);
             txtFilters = new JTextField();
-            // panel_3.add(txtFilters); окно для ввода фильтров
+             panel_3.add(txtFilters); // окно для ввода фильтров
             txtFilters.setColumns(10);
-            //JLabel lblArguments1 = new JLabel("\nНе работает");
-            // panel_3.add(lblArguments1);
         }
 
-        JPanel panel_4 = new JPanel(); /** создание панели для кнопки дамп файлов */
+        JPanel panel_4 = new JPanel(); // создание панели для кнопки дамп файлов */
         pnlSide.add(panel_4);
 
-        chkBoxDumpFile = new JCheckBox("Включить");
-        /** выбор дамп файла */
+        chkBoxDumpFile = new JCheckBox("Enable");
+        //* выбор дамп файла */
 
         JPanel panel_5 = new JPanel();
         pnlSide.add(panel_5);
         panel_5.setLayout(new FlowLayout(FlowLayout.LEADING, 5, 5));
-        //Font font = new Font("Inkulinati", Font.PLAIN, 11);
-      //  panel_5.setFont(font);
 
-        JLabel version = new JLabel("version : 1.0.11");
-       // panel_5.setForeground(Color.BLUE);
+        JLabel version = new JLabel("version : 1.2.5");
         panel_5.add(version);
 
 
-        chkBoxDumpFile.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-                if (chkBoxDumpFile.isSelected()) {
-                    JFileChooser chooser = new JFileChooser();
-                    //FileNameExtensionFilter filter = new FileNameExtensionFilter("pcap file", ".pcapFile");
-                    FileNameExtensionFilter filter = new FileNameExtensionFilter("pcap file", ".pcap");
+        chkBoxDumpFile.addActionListener(arg0 -> {
+            if (chkBoxDumpFile.isSelected()) {
+                JFileChooser chooser = new JFileChooser();
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("pcap file", ".pcap");
 
-                    chooser.setFileFilter(filter);
-                    int returnVal = chooser.showSaveDialog(frame);
-                    if (returnVal == JFileChooser.APPROVE_OPTION) {
-                        lblFilePath.setText(chooser.getSelectedFile().getAbsolutePath());
-                        if (!lblFilePath.getText().endsWith(".pcap")) {
-                            lblFilePath.setText(lblFilePath.getText() + ".pcap");
-                        }
-                    } else if (returnVal == JFileChooser.CANCEL_OPTION) {
-                        chkBoxDumpFile.setSelected(false);
-                        lblFilePath.setText("");
+                chooser.setFileFilter(filter);
+                int returnVal = chooser.showSaveDialog(frame);
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    lblFilePath.setText(chooser.getSelectedFile().getAbsolutePath());
+                    if (!lblFilePath.getText().endsWith(".pcap")) {
+                        lblFilePath.setText(lblFilePath.getText() + ".pcap");
                     }
-                } else {
+                } else if (returnVal == JFileChooser.CANCEL_OPTION) {
+                    chkBoxDumpFile.setSelected(false);
                     lblFilePath.setText("");
-                    return;
                 }
+            } else {
+                lblFilePath.setText("");
+                return;
             }
         });
 
@@ -241,32 +229,31 @@ public class PacketSniffer {
 
         JLabel lblDmp = new JLabel("Dump File");
         panel_4.add(lblDmp);
-        panel_4.add(chkBoxDumpFile); /** чекбокс */
+        panel_4.add(chkBoxDumpFile); // чекбокс */
         frame.getContentPane().add(pnlSide, BorderLayout.WEST);
 
 
-/** не работает */
-/** ОТКЛЮЧЕНО */
-        //
+
+
         JPanel panel = new JPanel();
-        //
+
         pnlSide.add(panel);
 
         JPanel pnlCenter = new JPanel();
         frame.getContentPane().add(pnlCenter, BorderLayout.CENTER);
         pnlCenter.setLayout(new BorderLayout(0, 0));
 
-        tableModel = new DefaultTableModel( /** вывод колонок с дальнейшим записыванием информации */
+        tableModel = new DefaultTableModel( //* вывод колонок с дальнейшим записыванием информации */
                 new Object[][]{
                 },
                 new String[]{
                         "Number", "Type", "Src", "Dst", "Data"
                 }
         );
-        /** вывод колонок*/
+        // вывод колонок*/
         tblOutput = new JTable();
         tblOutput.addMouseListener(new MouseAdapter() {
-            //@Override
+
             public void mouseClicked(MouseEvent arg0) {
                 int row = tblOutput.rowAtPoint(arg0.getPoint());
                 int col = tblOutput.columnAtPoint(arg0.getPoint());
@@ -277,13 +264,12 @@ public class PacketSniffer {
                 }
             }
         });
-        /** отображение горизонтальных линий разделяющих панель вывода логов пакетов */
+        //* отображение горизонтальных линий разделяющих панель вывода логов пакетов */
         tblOutput.setModel(tableModel);
         tblOutput.setShowHorizontalLines(true);
         JScrollPane scrollPane = new JScrollPane(tblOutput);
         pnlCenter.add(scrollPane, BorderLayout.CENTER);
-// не работает
-/** клик мышкой по пакету, выдает информацию*/
+// клик мышкой по пакету, выдает информацию*/
         pnlData = new JPanel();
         pnlCenter.add(pnlData, BorderLayout.SOUTH);
         pnlData.setLayout(new BorderLayout(0, 0));
@@ -291,9 +277,9 @@ public class PacketSniffer {
         txtData = new JTextArea();
         pnlData.add(txtData, BorderLayout.CENTER);
 
-        JButton btnCloseData = new JButton("Clear"); /** кнопка Close позволяет очистить колонки*/
+        JButton btnCloseData = new JButton("Clear"); //* кнопка clear позволяет очистить колонки*/
         btnCloseData.addMouseListener(new MouseAdapter() {
-            //  @Override
+
             public void mousePressed(MouseEvent arg0) {
 
                 if (tableModel.getRowCount() > 0) {
